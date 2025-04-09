bb2de11572c2bc0b598ce1b060a697a482eb655f
Zach Schmidt <zach@atpdevel.com>
2025-04-09 16:17:01 -0700

    Adding the payment table and payment creation button to the App

--- clientApp/src/App.jsx
+++ clientApp/src/App.jsx
@@ -1,18 +1,37 @@
 import React, { useState } from 'react';
 import InvoiceTable from './InvoiceTable.jsx';
 import CreateModal from './CreateModal.jsx';
+import PaymentTable from './PaymentTable.jsx';
+import CreatePaymentModal from './CreatePaymentModal.jsx';
 
 export default function App() {
 	const [invoices, setInvoices] = useState([]);
+	const [payments, setPayments] = useState([]);
 
 	const addInvoice = (newInvoice) => {
 		setInvoices((prevInvoices) => [...prevInvoices, newInvoice]);
+	};
+
+	const addPayment = (newPayment) => {
+		setPayments((prevPayments) => [...prevPayments, newPayment]);
+	};
+
+	const updateInvoice = (updatedInvoice) => {
+		setInvoices((prevInvoices) =>
+		    prevInvoices.map((invoice) =>
+		      invoice.invoiceNumber === updatedInvoice.invoiceNumber
+		        ? updatedInvoice
+		        : invoice
+		    )
+		  );
 	};
 
 	return (
 		<>
 			<InvoiceTable  invoices={invoices} setInvoices={setInvoices} />
 			<CreateModal  addInvoice={addInvoice} />
+			<PaymentTable payments={payments} setPayments={setPayments}/>
+			<CreatePaymentModal invoices={invoices} addPayment={addPayment} updateInvoice={updateInvoice} />
 		</>
 	);
 };
